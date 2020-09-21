7c7e28714a1d51db4704d13a752bee090ceae41f
phatdroid94 <phatdroid94@gmail.com>
2020-09-21 18:38:17 +0700

    finish

--- app/src/main/java/com/example/sharepreference13072020/MainActivity.java
+++ app/src/main/java/com/example/sharepreference13072020/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
         mSharedPreferences = getSharedPreferences("AppSharePreference",MODE_PRIVATE);
 
+        boolean isSave = mSharedPreferences.getBoolean("issave",false);
+        if (isSave){
+            String username = mSharedPreferences.getString("username","");
+            String password = mSharedPreferences.getString("password","");
+            mEdtUserName.setText(username);
+            mEdtPassWord.setText(password);
+            mCbSave.setChecked(isSave);
+        }
+
 
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,9 +56,15 @@ public class MainActivity extends AppCompatActivity {
 
                 if (username.equals("phat") && password.equals("123")){
                     Toast.makeText(MainActivity.this, "Dang nhap thanh cong", Toast.LENGTH_SHORT).show();
+                    mEditor = mSharedPreferences.edit();
                     if (mCbSave.isChecked()){
-
+                        mEditor.putString("username",username);
+                        mEditor.putString("password",password);
+                        mEditor.putBoolean("issave",true);
+                    }else{
+                        mEditor.clear();
                     }
+                    mEditor.apply();
                 }
             }
         });
